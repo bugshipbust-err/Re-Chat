09592252fdab08cb804f54d2c004746f24f94f76
bugshipbust-err <sivakrishan2701@gmail.com>
2025-11-23 13:50:25 +0530

    RAG functionality tested

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from langchain_ollama import ChatOllama
+from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain.agents import create_agent
-from langchain.agents.middleware import wrap_model_call, ModelRequest, ModelResponse
 from langchain.tools import tool
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.agents.middleware import wrap_model_call, ModelRequest, ModelResponse
 
 # ------------------------------------------------------------------------------------------------------------------------------- #
-
-llama = ChatOllama(model="llama3.1")
-gemma = ChatOllama(model="gemma3:12b")
-model = ChatOllama(model="gpt-oss:20b")
-
 system_prompt = """
-You are a helpful assistant who responds with personalized answers to the user's queries.
+You are a helpful assistant who responds with personalized responses to Neel's queries, who is the user.
 Keep your reponses really concise, short and straight to the point.
 """
 retriever_desc = """
 This is a user data retrieval tool that searches data relavant to you queries. Use this to answer queries that need additional information about the user
 """
+
+llama = ChatOllama(model="llama3.1")
+gemma = ChatOllama(model="gemma3:12b")
+model = ChatOllama(model="gpt-oss:20b")
+
+embeddings = OllamaEmbeddings(model="embeddinggemma:300m")
+vector_store = Chroma(
+        collection_name="chroma_db",
+        embedding_function=embeddings,
+        persist_directory="./chroma_langchain_db",
+    )
+text_sep_splitter = RecursiveCharacterTextSplitter(
+        separators=".",
+        keep_separator=True,
+        chunk_size=1,               # force splitting at separator
+        chunk_overlap=0,
+    )
 
 # ------------------------------------------------------------------------------------------------------------------------------- #
 
---
+++ rag.py
@@ -0,0 +1,51 @@
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+# ------------------------------------------------------------------------------------------------------------------------------ #
+
+def load_txt(f_name):
+    with open(f_name) as f:
+        text = f.read()
+    return text 
+
+# ------------------------------------------------------------------------------------------------------------------------------ #
+
+embeddings = OllamaEmbeddings(model="embeddinggemma:300m")
+
+vector_store = Chroma(
+        collection_name="chroma_db",
+        embedding_function=embeddings,
+        persist_directory="./chroma_langchain_db",
+    )
+
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=250,
+        chunk_overlap=50,
+        add_start_index=True,
+    )
+text_sep_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        keep_separator=False,
+        chunk_size=1,               # force splitting at separator
+        chunk_overlap=0,
+    )
+
+# ------------------------------------------------------------------------------------------------------------------------------ #
+
+sample = load_txt(f_name="neel_text.txt")
+text_splits = text_sep_splitter.split_text(sample)
+print(text_splits[:3])
+
+documents = [Document(page_content=text_split) for text_split in text_splits]
+document_ids = vector_store.add_documents(documents=documents)
+
+print(len(document_ids) == len(text_splits))
+print(document_ids[:2])
+
+query = "neel nanda"
+retrieved_docs = vector_store.similarity_search(query, k=2)
+
+print(retrieved_docs)
+
+
